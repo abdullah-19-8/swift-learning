75429f3432b922ca7b1b42e05cce43345a372979
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2023-12-24 18:54:31 +0300

    Day 5 – if, switch, and the ternary operator

--- swift-consept.playground/Contents.swift
+++ swift-consept.playground/Contents.swift
@@ -1,46 +1,293 @@
+let score = 85
 
-//let surname = "Lasso"
-//var score = 0
+if score > 80 {
+    print("Great job!")
+}
 
-let surname : String = "Lasso"
-var scores : Int = 0
-var score : Double = 0
+let speed = 88
+let percentage = 85
+let age = 18
 
-//String holds text:
-let playerName: String = "Roy"
+if speed >= 88 {
+    print("Where we're going we don't need roads.")
+}
 
-//Int holds whole numbers:
-var luckyNumber: Int = 13
+if percentage < 85 {
+    print("Sorry, you failed the test.")
+}
 
-//Double holds decimal numbers:
-let pi: Double = 3.141
+if age >= 18 {
+    print("You're eligible to vote")
+}
 
-//Bool holds either true or false:
-var isAuthenticated: Bool = true
+let ourName = "Dave Lister"
+let friendName = "Arnold Rimmer"
 
-//Array holds lots of different values, all in the order you add them. This must be specialized, such as [String]:
-var album : [String] = ["Abdullah", "Asma","Test"]
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
 
-//Dictionary holds lots of different values, where you get to decide how data should be accessed. This must be specialized, such as [String: Int]:
-var user: [String: String] = ["id": "@twostraws"]
-
-//Set holds lots of different values, but stores them in an order that’s optimized for checking what it contains. This must be specialized, such as Set<String>:
-var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
-
-var teams: [String] = [String]()
-
-var cities: [String] = []
-
-var clues = [String]()
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
 
 
-print("---------------------------------------------")
+// Make an array of 3 numbers
+var numbers = [1, 2, 3]
 
-// Checkpoint 2
+// Add a 4th
+numbers.append(4)
 
-let names = ["Abdullah","Asma","test","Asma"]
+// If we have over 3 items
+if numbers.count > 3 {
+    // Remove the oldest number
+    numbers.remove(at: 0)
+}
 
-let uniqueNames = Set(names)
+// Display the result
+print(numbers)
 
-print(names.count, uniqueNames.count)
+let country = "Canada"
 
+if country == "Australia" {
+    print("G'day!")
+}
+
+let name = "Taylor Swift"
+
+if name != "Anonymous" {
+    print("Welcome, \(name)")
+}
+
+// Create the username variable
+var username = "taylorswift13"
+
+// If `username` contains an empty string
+if username == "" {
+    // Make it equal to "Anonymous"
+    username = "Anonymous"
+}
+
+// Now print a welcome message
+print("Welcome, \(username)!")
+
+if username.count == 0 {
+    username = "Anonymous"
+}
+
+if username.isEmpty == true {
+    username = "Anonymous"
+}
+
+if username.isEmpty {
+    username = "Anonymous"
+}
+
+let firstName = "Paul"
+let secondName = "Sophie"
+
+let firstAge = 40
+let secondAge = 10
+
+print(firstName == secondName)
+print(firstName != secondName)
+print(firstName < secondName)
+print(firstName >= secondName)
+
+print(firstAge == secondAge)
+print(firstAge != secondAge)
+print(firstAge < secondAge)
+print(firstAge >= secondAge)
+
+enum Sizes: Comparable {
+    case small
+    case medium
+    case large
+}
+
+let first = Sizes.small
+let second = Sizes.large
+print(first < second)
+// That will print “true”, because small comes before large in the enum case list.
+
+print("-----------------------------------------------------")
+
+// How to check multiple conditions
+
+let ages = 16
+
+if ages >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let a = false
+let b = true
+
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+let temp = 25
+
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day.")
+    }
+}
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
+}
+
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
+print("-----------------------------------------------------")
+
+// How to use switch statements to check multiple conditions
+
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+if forecast == .sun {
+    print("It should be a nice day.")
+} else if forecast == .rain {
+    print("Pack an umbrella.")
+} else if forecast == .wind {
+    print("Wear something warm")
+} else if forecast == .snow {
+    print("School is cancelled.")
+} else {
+    print("Our forecast generator is broken!")
+}
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella.")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken!")
+}
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+let day = 5
+print("My true love gave to me…")
+
+switch day {
+case 5:
+    print("5 golden rings")
+case 4:
+    print("4 calling birds")
+case 3:
+    print("3 French hens")
+case 2:
+    print("2 turtle doves")
+default:
+    print("A partridge in a pear tree")
+}
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
+
+print("-----------------------------------------------------")
+
+// How to use the ternary conditional operator for quick tests
+
+let agee = 18
+let canVote = agee >= 18 ? "Yes" : "No"
+
+let hour = 23
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.dark
+
+let background = theme == .dark ? "black" : "white"
+print(background)
+
+let hours = 23
+print(hours < 12 ? "It's before noon" : "It's after noon")
+//print(
+//    if hour < 12 {
+//        "It's before noon"
+//    } else {
+//        "It's after noon"
+//    }
+//)
+
+if hour < 12 {
+    print("It's before noon")
+} else {
+    print("It's after noon")
+}
