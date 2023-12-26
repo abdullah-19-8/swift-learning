4d40236d49c2690d33e55f7d4f57bc0b70fa7705
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2023-12-26 07:17:33 +0300

    Day 8 – default values, throwing functions, and checkpoint 4

--- swift-consept.playground/Contents.swift
+++ swift-consept.playground/Contents.swift
@@ -1,126 +1,120 @@
-import Foundation
+//func printTimesTables(for number: Int, end: Int) {
+//    for i in 1...end {
+//        print("\(i) x \(number) is \(i * number)")
+//    }
+//}
+//
+//printTimesTables(for: 5, end: 20)
 
-func showWelcome() {
-    print("Welcome to my app!")
-    print("By default This prints out a conversion")
-    print("chart from centimeters to inches, but you")
-    print("can also set a custom range if you want.")
-}
-
-func printTimesTables(number: Int) {
-    for i in 1...12 {
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
         print("\(i) x \(number) is \(i * number)")
     }
 }
 
-printTimesTables(number: 5)
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
 
-func test(text: String){
-    print(text)
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+characters.removeAll(keepingCapacity: true)
+print(characters.count)
+
+print("---------------------------------------------------")
+
+// How to handle errors in functions
+
+enum PasswordError: Error {
+    case short, obvious
 }
 
-test(text: "test")
-
-func rollDice() -> Int {
-    return Int.random(in: 1...6)
-}
-
-let result = rollDice()
-print(result)
-
-func areLettersIdentical(string1: String, string2:String) -> Bool{
-     string1.sorted() == string2.sorted()
-}
-
-areLettersIdentical(string1: "abc", string2: "cab")
-areLettersIdentical(string1: "test", string2: "Rest")
-
-func pythagoras(a: Double, b: Double) -> Double {
-    sqrt(a * a + b * b)
-}
-
-let c = pythagoras(a: 3, b: 4)
-print(c)
-
-print("--------------------------------------------------")
-
-// How to return multiple values from functions
-
-//func getUser() -> [String] {
-//    ["Taylor", "Swift"]
-//}
-
-
-//let user = getUser()
-//print("Name: \(user[0]) \(user[1])")
-
-
-//func getUser() -> [String: String] {
-//    [
-//        "firstName": "Taylor",
-//        "lastName": "Swift"
-//    ]
-//}
-//
-//let user = getUser()
-//print("Name: \(user["firstName", default: "Anonymous"]) \(user["lastName", default: "Anonymous"])")
-
-//func getUser() -> (firstName: String, lastName: String) {
-//    (firstName: "Taylor", lastName: "Swift")
-//}
-
-func getUser() -> (firstName: String, lastName: String) {
-    ("Taylor", "Swift")
-}
-
-let user = getUser()
-print("Name: \(user.firstName) \(user.lastName)")
-print("Name: \(user.0) \(user.1)")
-
-let (firstName, _) = getUser()
-print("Name: \(firstName)")
-
-print("--------------------------------------------------")
-
-// How to customize parameter labels
-
-func rollDice(sides: Int, count: Int) -> [Int] {
-    // Start with an empty array
-    var rolls = [Int]()
-
-    // Roll as many dice as needed
-    for _ in 1...count {
-        // Add each result to our array
-        let roll = Int.random(in: 1...sides)
-        rolls.append(roll)
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
     }
-
-    // Send back all the rolls
-    return rolls
-}
-
-let rolls = rollDice(sides: 6, count: 4)
-
-print(rolls)
-
-//func isUppercase(string: String) -> Bool {
-//    string == string.uppercased()
-//}
-//
-//let string = "HELLO, WORLD"
-//let resultt = isUppercase(string: string)
-
-func isUppercase(_ string: String) -> Bool {
-    string == string.uppercased()
-}
-
-let string = "HELLO, WORLD"
-let resultt = isUppercase(string)
-
-func printTimesTables(for number: Int) {
-    for i in 1...12 {
-        print("\(i) x \(number) is \(i * number)")
+    
+    if password == "12345"{
+        throw PasswordError.obvious
     }
+    
+    if password.count < 8 {
+           return "OK"
+       } else if password.count < 10 {
+           return "Good"
+       } else {
+           return "Excellent"
+       }
+}
+do {
+    let result = try checkPassword("1234")
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
 }
 
-printTimesTables(for: 5)
+do {
+    let result = try checkPassword("12345")
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
+
+do {
+    let result = try checkPassword("1234567")
+    print("Password rating: \(result)")
+} catch {
+    print("There was an error.")
+}
+do {
+    let result = try checkPassword("123456789")
+    print("Password rating: \(result)")
+} catch {
+    print("There was an error.")
+}
+do {
+    let result = try checkPassword("1234567890")
+    print("Password rating: \(result)")
+} catch {
+    print("There was an error.")
+}
+
+// Checkpoint 4
+
+enum NumberError : Error{
+    case lessOrGreaterThan, noSqrt
+}
+
+func squerRoot(of number: Int) throws -> Int{
+    if number < 1 || number > 10000{
+        throw NumberError.lessOrGreaterThan
+    }
+    for num in 1...number {
+        if(num * num == number){
+            return num
+        }
+    }
+    throw NumberError.noSqrt
+}
+
+let number = 10000
+
+do{
+
+    let result = try squerRoot(of: number)
+    print(result)
+
+} catch NumberError.lessOrGreaterThan {
+    print("number should be in range 1 to 1000")
+} catch NumberError.noSqrt{
+    print("this number has no sqrt")
+} catch{
+    print("there are an error")
+}
