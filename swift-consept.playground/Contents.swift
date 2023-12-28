c040df37d49e91463dec9fa5cbf1bff673a17176
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2023-12-28 05:57:34 +0300

    Day 13 – protocols, extensions, and checkpoint 8

--- swift-consept.playground/Contents.swift
+++ swift-consept.playground/Contents.swift
@@ -1,164 +1,261 @@
-class Game {
-    var score = 0 {
-        didSet {
-            print("Score is now \(score)")
+import Foundation
+
+struct Car: Vehicles {
+    let name = "Car"
+    var currentPassengers = 1
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km.")
+    }
+
+    func openSunroof() {
+        print("It's a nice day!")
+    }
+}
+
+func commute(distance: Int, using vehicle: Vehicles) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+let car = Car()
+commute(distance: 100, using: car)
+
+struct Bicycle: Vehicles {
+    let name = "Bicycle"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km.")
+    }
+}
+
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
+protocol Vehicles {
+    var name: String { get }
+    var currentPassengers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+
+func getTravelEstimates(using vehicles: [Vehicles], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
+    }
+}
+
+getTravelEstimates(using: [car, bike], distance: 150)
+
+//
+//struct Book {
+//    var name: String
+//}
+//
+//func buy(_ book: Book) {
+//    print("I'm buying \(book.name)")
+//}
+
+protocol Purchaseable {
+    var name: String { get set }
+}
+
+struct Book: Purchaseable {
+    var name: String
+    var author: String
+}
+
+struct Movie: Purchaseable {
+    var name: String
+    var actors: [String]
+}
+
+struct Cars: Purchaseable {
+    var name: String
+    var manufacturer: String
+}
+
+struct Coffee: Purchaseable {
+    var name: String
+    var strength: Int
+}
+
+func buy(_ item: Purchaseable) {
+    print("I'm buying \(item.name)")
+}
+
+// How to use opaque return types
+
+//func getRandomNumber() -> Int {
+//    Int.random(in: 1...6)
+//}
+//
+//func getRandomBool() -> Bool {
+//    Bool.random()
+//}
+
+func getRandomNumber() -> some Equatable {
+    Int.random(in: 1...6)
+}
+
+func getRandomBool() -> some Equatable {
+    Bool.random()
+}
+
+// extensions
+
+var quote = "   The truth is rarely pure and never simple   "
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim(){
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+let trimmed2 = quote.trimmed()
+
+print(trimmed)
+print(trimmed2)
+
+quote.trim()
+
+
+let lyrics = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+
+print(lyrics.lines.count)
+
+struct Books {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+
+extension Books{
+    init(title: String, pageCount: Int) {
+            self.title = title
+            self.pageCount = pageCount
+            self.readingHours = pageCount / 50
         }
+}
+
+let lotr = Books(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
+
+// How to create and use protocol extensions
+
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isEmpty == false {
+    print("Guest count: \(guests.count)")
+}
+
+if !guests.isEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+extension Collection {
+    var isNotEmpty: Bool{
+        isEmpty == false
     }
 }
 
-var newGame = Game()
-newGame.score += 10
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
 
-// How to make one class inherit from another
 
-class Employee {
-    let hours: Int
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
 
-    init(hours: Int) {
-        self.hours = hours
-    }
-    func printSummary() {
-        print("I work \(hours) hours a day.")
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
     }
 }
 
-class Developer: Employee {
-    func work() {
-        print("I'm writing code for \(hours) hours.")
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
+
+let numbers = [4, 8, 15, 16]
+let allEven = numbers.allSatisfy { $0.isMultiple(of: 2) }
+
+let numbers2 = Set([4, 8, 15, 16])
+let allEven2 = numbers2.allSatisfy { $0.isMultiple(of: 2) }
+
+let numbers3 = ["four": 4, "eight": 8, "fifteen": 15, "sixteen": 16]
+let allEven3 = numbers3.allSatisfy { $0.value.isMultiple(of: 2) }
+
+print(allEven)
+print(allEven2)
+print(allEven3)
+
+
+// checkpoint 8
+
+protocol Building{
+    var roomNumbers: Int { get }
+    var cost: Int { get }
+    var agent: String { get }
+    func summary()
+}
+
+extension Building {
+    func summary() {
+        print("Room numbers: \(roomNumbers) Cost: \(cost) Agent: \(agent)")
     }
+}
+
+struct House: Building{
+    var roomNumbers: Int
     
-    override func printSummary() {
-        print("I'm a developer who will sometimes work \(hours) hours a day, but other times spend hours arguing about whether code should be indented using tabs or spaces.")
-    }
+    var cost: Int
     
-}
-
-class Manager: Employee {
-    func work() {
-        print("I'm going to meetings for \(hours) hours.")
-    }
-}
-
-let robert = Developer(hours: 8)
-let joseph = Manager(hours: 10)
-robert.work()
-joseph.work()
-
-let novall = Developer(hours: 8)
-novall.printSummary()
-
-// How to add initializers for classes
-
-class Vehicle {
-    let isElectric: Bool
-
-    init(isElectric: Bool) {
-        self.isElectric = isElectric
-    }
-}
-
-class Car: Vehicle {
-    let isConvertible: Bool
-
-    init(isElectric: Bool, isConvertible: Bool) {
-        self.isConvertible = isConvertible
-        super.init(isElectric: isElectric)
-    }
-}
-
-// How to create a deinitializer for a class
-
-class User {
-    let id: Int
-
-    init(id: Int) {
-        self.id = id
-        print("User \(id): I'm alive!")
-    }
-
-    deinit {
-        print("User \(id): I'm dead!")
-    }
-}
-
-for i in 1...3 {
-    let user = User(id: i)
-    print("User \(user.id): I'm in control!")
-}
-
-var users = [User]()
-
-for i in 1...3 {
-    let user = User(id: i)
-    print("User \(user.id): I'm in control!")
-    users.append(user)
-}
-
-print("Loop is finished!")
-users.removeAll()
-print("Array is clear!")
-
-// How to work with variables inside classes
-
-class Users {
-    var name = "Paul"
-}
-
-var user = Users()
-user.name = "Taylor"
-user = Users()
-print(user.name)
-
-// Checkpoint 7
-
-class Animal{
-    let leg: Int
+    var agent: String
     
-    init(leg: Int) {
-        self.leg = leg
-    }
     
 }
 
-class Dog: Animal{
-    func speak(){
-        print("wow")
-    }
-}
+let house = House(roomNumbers: 3, cost: 500, agent: "Abdullah")
 
-class Cat: Animal{
-    let isTame: Bool
+house.summary()
+
+struct Office: Building{
+    var roomNumbers: Int
     
-    init(isTame: Bool) {
-        self.isTame = isTame
-        super.init(leg: 4)
-    }
-    func speak(){
-        print("meow")
-    }
+    var cost: Int
+    
+    var agent: String
+    
+    
 }
-
-class Corgi: Dog{
-    override func speak() {
-        print("7apa 7ap")
-    }
-}
-
-class Poodle: Dog{
-    override func speak() {
-        print("chgha")
-    }
-}
-
-class Persian: Cat{
-    override func speak() {
-        print("meow 2")
-    }
-}
-
-class Lion: Cat{
-    override func speak() {
-        print("meow 3")
-    }
-}
-
