af58a1f40d2e0e268723daf2922198dbd36bcbe0
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2023-12-31 05:14:09 +0300

    layout designing done

--- UIKit-Project2/UIKit-Project2/ViewController.swift
+++ UIKit-Project2/UIKit-Project2/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
