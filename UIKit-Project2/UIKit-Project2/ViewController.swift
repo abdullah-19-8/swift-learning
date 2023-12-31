c9d05d62dd56f255df766922f1561df918bbe19e
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2023-12-31 06:03:07 +0300

    random flag generate

--- UIKit-Project2/UIKit-Project2/ViewController.swift
+++ UIKit-Project2/UIKit-Project2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,7 +43,7 @@ class ViewController: UIViewController {
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
-        
+            
         button1.layer.borderColor = UIColor.lightGray.cgColor
         button2.layer.borderColor = UIColor.lightGray.cgColor
         button3.layer.borderColor = UIColor.lightGray.cgColor
@@ -50,9 +51,14 @@ class ViewController: UIViewController {
     }
     
     func askQuestions() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
 
 
