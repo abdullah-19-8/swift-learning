ec34a13d660f48fe1e573819714ab34fa7bc7459
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2024-01-02 17:57:16 +0300

    project 4 setting up done

---
+++ UIKit-Project4/UIKit-Project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-Project4
+//
+//  Created by Abdulla Ahmad on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
