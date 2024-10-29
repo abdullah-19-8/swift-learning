97a3a80568cd534be5e885c1bed33fc9320f87fb
Abdullah <abdullaahmadsheikh@gmail.com>
2024-10-30 01:17:38 +0300

    New SwiftUI Project added

---
+++ Swift-UI/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Abdulla Ahmad on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
